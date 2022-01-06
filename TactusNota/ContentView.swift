d180a4aa188391adf8281a3f3d80a79c74dde195
jithin8mathew <jithinjosemathewoo8@gmail.com>
2022-01-06 17:54:22 -0600

    Initial commit

--- TactusNota/ContentView.swift
+++ TactusNota/ContentView.swift
@@ -9,8 +9,86 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            HStack{
+                Button(action: {
+                                    }, label: {
+                                        Text("Images")
+                                        .font(.system(.title2))
+                                        .frame(width: 97, height: 40)
+                                        .foregroundColor(Color.white)
+                                        .padding(.bottom, 7)
+                                    })
+                                    .background(Color.blue)
+                                    .cornerRadius(38.5)
+                                    .shadow(color: Color.black.opacity(0.3),
+                                            radius: 3,
+                                            x: 3,
+                                            y: 3)
+                Button(action: {
+                                    }, label: {
+                                        Text("Save directory")
+                                        .font(.system(.title2))
+                                        .frame(width: 160, height: 40)
+                                        .foregroundColor(Color.white)
+                                        .padding(.bottom, 7)
+                                    })
+                                    .background(Color.blue)
+                                    .cornerRadius(38.5)
+                                    .shadow(color: Color.black.opacity(0.3),
+                                            radius: 3,
+                                            x: 3,
+                                            y: 3)
+                Button(action: {
+                                    }, label: {
+                                        Text("Previous")
+                                        .font(.system(.title2))
+                                        .frame(width: 97, height: 40)
+                                        .foregroundColor(Color.white)
+                                        .padding(.bottom, 7)
+                                    })
+                                    .background(Color.blue)
+                                    .cornerRadius(38.5)
+                                    .shadow(color: Color.black.opacity(0.3),
+                                            radius: 3,
+                                            x: 3,
+                                            y: 3)
+                Button(action: {
+                                    }, label: {
+                                        Text("Next")
+                                        .font(.system(.title2))
+                                        .frame(width: 97, height: 40)
+                                        .foregroundColor(Color.white)
+                                        .padding(.bottom, 7)
+                                    })
+                                    .background(Color.blue)
+                                    .cornerRadius(38.5)
+                                    .shadow(color: Color.black.opacity(0.3),
+                                            radius: 3,
+                                            x: 3,
+                                            y: 3)
+                Button(action: {
+                                    }, label: {
+                                        Text("Save")
+                                        .font(.system(.title2))
+                                        .frame(width: 97, height: 40)
+                                        .foregroundColor(Color.white)
+                                        .padding(.bottom, 7)
+                                    })
+                                    .background(Color.blue)
+                                    .cornerRadius(38.5)
+                                    .shadow(color: Color.black.opacity(0.3),
+                                            radius: 3,
+                                            x: 3,
+                                            y: 3)
+                
+            }
+            
+            Image("test1")
+                .resizable()
+        }
+        
+
     }
 }
 
