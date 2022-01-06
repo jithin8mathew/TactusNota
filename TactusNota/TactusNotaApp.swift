e8fe3b9987517726cdda08ac0142ca03b32639ef
jithin8mathew <jithinjosemathewoo8@gmail.com>
2022-01-06 16:19:13 -0600

    Initial Commit

---
+++ TactusNota/TactusNotaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TactusNotaApp.swift
+//  TactusNota
+//
+//  Created by Jithin Mathew on 1/6/22.
+//
+
+import SwiftUI
+
+@main
+struct TactusNotaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
