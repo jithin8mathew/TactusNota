a973dd9db2ecc4dc574a34b6aab3a61e5bd50036
jithin8mathew <jithinjosemathewoo8@gmail.com>
2022-06-19 16:28:41 -0500

    imagePicker Work in progress

--- TactusNota/ContentView.swift
+++ TactusNota/ContentView.swift
@@ -11,86 +11,94 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack{
-            HStack{
-                Button(action: {
-                                    }, label: {
-                                        Text("Images")
-                                        .font(.system(.title2))
-                                        .frame(width: 97, height: 40)
-                                        .foregroundColor(Color.white)
-                                        .padding(.bottom, 7)
-                                    })
-                                    .background(Color.blue)
-                                    .cornerRadius(38.5)
-                                    .shadow(color: Color.black.opacity(0.3),
-                                            radius: 3,
-                                            x: 3,
-                                            y: 3)
-                Button(action: {
-                                    }, label: {
-                                        Text("Save directory")
-                                        .font(.system(.title2))
-                                        .frame(width: 160, height: 40)
-                                        .foregroundColor(Color.white)
-                                        .padding(.bottom, 7)
-                                    })
-                                    .background(Color.blue)
-                                    .cornerRadius(38.5)
-                                    .shadow(color: Color.black.opacity(0.3),
-                                            radius: 3,
-                                            x: 3,
-                                            y: 3)
-                Button(action: {
-                                    }, label: {
-                                        Text("Previous")
-                                        .font(.system(.title2))
-                                        .frame(width: 97, height: 40)
-                                        .foregroundColor(Color.white)
-                                        .padding(.bottom, 7)
-                                    })
-                                    .background(Color.blue)
-                                    .cornerRadius(38.5)
-                                    .shadow(color: Color.black.opacity(0.3),
-                                            radius: 3,
-                                            x: 3,
-                                            y: 3)
-                Button(action: {
-                                    }, label: {
-                                        Text("Next")
-                                        .font(.system(.title2))
-                                        .frame(width: 97, height: 40)
-                                        .foregroundColor(Color.white)
-                                        .padding(.bottom, 7)
-                                    })
-                                    .background(Color.blue)
-                                    .cornerRadius(38.5)
-                                    .shadow(color: Color.black.opacity(0.3),
-                                            radius: 3,
-                                            x: 3,
-                                            y: 3)
-                Button(action: {
-                                    }, label: {
-                                        Text("Save")
-                                        .font(.system(.title2))
-                                        .frame(width: 97, height: 40)
-                                        .foregroundColor(Color.white)
-                                        .padding(.bottom, 7)
-                                    })
-                                    .background(Color.blue)
-                                    .cornerRadius(38.5)
-                                    .shadow(color: Color.black.opacity(0.3),
-                                            radius: 3,
-                                            x: 3,
-                                            y: 3)
+        ZStack{
+        NavigationView{
+            
+            VStack{
+            
+                HStack{
+                    Button(action: {
+                                        }, label: {
+                                            Text("Images")
+                                            .font(.system(.title2))
+                                            .frame(width: 97, height: 40)
+                                            .foregroundColor(Color.white)
+                                            .padding(.bottom, 7)
+                                        })
+                                        .background(Color.blue)
+                                        .cornerRadius(38.5)
+                                        .shadow(color: Color.black.opacity(0.3),
+                                                radius: 3,
+                                                x: 3,
+                                                y: 3)
+                    Button(action: {
+                                        }, label: {
+                                            Text("Save directory")
+                                            .font(.system(.title2))
+                                            .frame(width: 160, height: 40)
+                                            .foregroundColor(Color.white)
+                                            .padding(.bottom, 7)
+                                        })
+                                        .background(Color.blue)
+                                        .cornerRadius(38.5)
+                                        .shadow(color: Color.black.opacity(0.3),
+                                                radius: 3,
+                                                x: 3,
+                                                y: 3)
+                    Button(action: {
+                                        }, label: {
+                                            Text("Previous")
+                                            .font(.system(.title2))
+                                            .frame(width: 97, height: 40)
+                                            .foregroundColor(Color.white)
+                                            .padding(.bottom, 7)
+                                        })
+                                        .background(Color.blue)
+                                        .cornerRadius(38.5)
+                                        .shadow(color: Color.black.opacity(0.3),
+                                                radius: 3,
+                                                x: 3,
+                                                y: 3)
+                    Button(action: {
+                                        }, label: {
+                                            Text("Next")
+                                            .font(.system(.title2))
+                                            .frame(width: 97, height: 40)
+                                            .foregroundColor(Color.white)
+                                            .padding(.bottom, 7)
+                                        })
+                                        .background(Color.blue)
+                                        .cornerRadius(38.5)
+                                        .shadow(color: Color.black.opacity(0.3),
+                                                radius: 3,
+                                                x: 3,
+                                                y: 3)
+                    Button(action: {
+                                        }, label: {
+                                            Text("Save")
+                                            .font(.system(.title2))
+                                            .frame(width: 97, height: 40)
+                                            .foregroundColor(Color.white)
+                                            .padding(.bottom, 7)
+                                        })
+                                        .background(Color.blue)
+                                        .cornerRadius(38.5)
+                                        .shadow(color: Color.black.opacity(0.3),
+                                                radius: 3,
+                                                x: 3,
+                                                y: 3)
+                    
+                }
                 
+                Image("test1")
+                    .resizable()
             }
             
-            Image("test1")
-                .resizable()
-        }
-        
+           
 
+        }.navigationViewStyle(StackNavigationViewStyle()) // end of Navigation View// end of navigation view
+        .padding(.all, 0)
+        } // end of zstack
     }
 }
 
---
+++ TactusNota/ImagePicker.swift
@@ -0,0 +1,56 @@
+//
+//  ImagePicker.swift
+//  TactusNota
+//
+//  Created by Jithin Mathew on 6/19/22.
+//
+
+import SwiftUI
+import UIKit
+
+struct ImagePicker: UIViewControllerRepresentable {
+    
+    @Binding var showPicker: Bool
+    @Binding var imageData: Data
+    
+    func makeCoordinator() -> Coordinator {
+        return ImagePicker.Coordinator(parent:self)
+    }
+    
+    func makeUIViewController(context: Context) -> some UIImagePickerController {
+        let controller = UIImagePickerController()
+        controller.sourceType = .photoLibrary
+        controller.delegate = context.coordinator
+        
+        return controller
+    }
+    
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {
+        <#code#>
+    }
+    
+    class coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+        var parent: ImagePicker
+        
+        init(parent: ImagePicker) {
+            self.parent = parent
+        }
+        
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            if let imageData = (info[.originalImage] as? UIImage)?.pngData(){
+                parent.imageData = imageData
+                parent.showPicker.toggle()
+            }
+        }
+        
+        func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+            parent.showPicker.toggle()
+        }
+    }
+}
+
+//struct ImagePicker_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ImagePicker()
+//    }
+//}
