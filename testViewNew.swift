01f0461bf66f027e4871471991bc030256d7baa8
jithin8mathew <jithinjosemathewoo8@gmail.com>
2022-08-26 00:11:44 -0500

    initial commit

--- testViewNew.swift
+++ testViewNew.swift
@@ -13,6 +13,33 @@ struct testViewNew: View {
         case inactive
         case pressing
         case dragging(translation: CGSize)
+        
+        var translation: CGSize {
+            switch self {
+            case .inactive, .pressing:
+                return .zero
+            case .dragging(let translation):
+                return translation
+            }
+        }
+        
+        var isActive: Bool {
+            switch self {
+            case .inactive:
+                return false
+            case .pressing, .dragging:
+                return true
+            }
+        }
+        
+        var isDragging: Bool {
+            switch self {
+            case .inactive, .pressing:
+                return false
+            case .dragging:
+                return true
+            }
+        }
     }
     
     @State var startLoc = CGPoint.zero // start location of the coordinate the user clicks
