ab360eb63a41178d9054a67bb8445c34f3c97558
jithin8mathew <jithinjosemathewoo8@gmail.com>
2022-08-27 22:14:03 -0500

    small changes towards making bbox update in realtime.

--- testViewNew.swift
+++ testViewNew.swift
@@ -120,6 +120,11 @@ struct testViewNew: View {
                                     if value.startLocation.x >=  (bboxCoordinates[0]-15) && value.startLocation.x <= ( bboxCoordinates[0] +  15)  && value.startLocation.y >=  (bboxCoordinates[1] - 15) && value.startLocation.y <= ( bboxCoordinates[1] +  15){
                                         rectCircleData[boundingBoxID] = nil
                                         rectData[boundingBoxID] = [bboxCoordinates[0] - abs(value.location.x - startLoc.x), bboxCoordinates[1] - abs(value.location.y - startLoc.y), bboxCoordinates[2] + abs(value.location.x - startLoc.x), bboxCoordinates[3] + abs(value.location.y - startLoc.y)]
+                                        
+//                                        startLoc.x = bboxCoordinates[0] - abs(value.location.x - startLoc.x)
+//                                        startLoc.y = bboxCoordinates[1] - abs(value.location.y - startLoc.y)
+//                                        contWidth = bboxCoordinates[2] + abs(value.location.x - startLoc.x)
+//                                        contHeight = bboxCoordinates[3] + abs(value.location.y - startLoc.y)
 //                                        print(bboxCoordinates[0] + value.startLocation.x, bboxCoordinates[1] + value.startLocation.y, bboxCoordinates[2], bboxCoordinates[3])
                                         rectCircleData[boundingBoxID] = [value.startLocation.x, value.startLocation.y, bboxCoordinates[2], bboxCoordinates[3]]
                                         print(rectCircleData[boundingBoxID])
