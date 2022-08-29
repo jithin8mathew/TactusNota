8e62dec40ba69d2de25c9b07c91b2ba72e19ac15
jithin8mathew <jithinjosemathewoo8@gmail.com>
2022-08-29 11:09:40 -0500

    still trying

--- testViewNew.swift
+++ testViewNew.swift
@@ -102,23 +102,20 @@ struct testViewNew: View {
                             
                             rectData[boundingBoxID] = [bboxCoordinates[0] - (-1 * (value.location.x - startLoc.x)), bboxCoordinates[1] - (-1 * (value.location.y - startLoc.y)), bboxCoordinates[2] + (-1 * (value.location.x - startLoc.x)), bboxCoordinates[3] + (-1 * (value.location.y - startLoc.y))]
                             
-//                            rectCircleData[boundingBoxID]=[bboxCoordinates[0] - (-1 * (value.location.x - startLoc.x)), bboxCoordinates[1] - (-1 * (value.location.y - startLoc.y)), bboxCoordinates[2] + (-1 * (value.location.x - startLoc.x)), bboxCoordinates[3] + (-1 * (value.location.y - startLoc.y))]
-                            //                                        startLoc.x = bboxCoordinates[0] - (-1 * (value.location.x - startLoc.x))
-                            //                                        startLoc.y = bboxCoordinates[1] - (-1 * (value.location.y - startLoc.y))
-                            //                                        contWidth = bboxCoordinates[2] + (-1 * (value.location.x - startLoc.x))
-                            //                                        contHeight = bboxCoordinates[3] + (-1 * (value.location.y - startLoc.y))
+                            //                            rectCircleData[boundingBoxID]=[bboxCoordinates[0] - (-1 * (value.location.x - startLoc.x)), bboxCoordinates[1] - (-1 * (value.location.y - startLoc.y)), bboxCoordinates[2] + (-1 * (value.location.x - startLoc.x)), bboxCoordinates[3] + (-1 * (value.location.y - startLoc.y))]
+//                            startLoc.x = bboxCoordinates[0] - (-1 * (value.location.x - startLoc.x))
+//                            startLoc.y = bboxCoordinates[1] - (-1 * (value.location.y - startLoc.y))
+//                            contWidth = bboxCoordinates[2] + (-1 * (value.location.x - startLoc.x))
+//                            contHeight = bboxCoordinates[3] + (-1 * (value.location.y - startLoc.y))
                         }
-                        //                                else if value.startLocation.x >=  ((bboxCoordinates[0]-15) + bboxCoordinates[2]) && value.startLocation.x <= ((bboxCoordinates[0] + 15) + bboxCoordinates[2])  && value.startLocation.y >=  (bboxCoordinates[1] - 15) && value.startLocation.y <=  (bboxCoordinates[1] +  15){
-                        //
-                        //                                    rectData[boundingBoxID] = [bboxCoordinates[0] - (-1 * (value.location.x - startLoc.x)), bboxCoordinates[1] - (-1 * (value.location.x - startLoc.x)), bboxCoordinates[2] , bboxCoordinates[3]]
-                        //                                }
-                        //                                else if value.startLocation.x >=  (bboxCoordinates[0]-15) && value.startLocation.x <= (bboxCoordinates[0] + 15)   && value.startLocation.y >=  ((bboxCoordinates[1] - 15) + bboxCoordinates[3]) && value.startLocation.y <=  ((bboxCoordinates[1] +  15) + bboxCoordinates[3]){
-                        //                                    rectData[boundingBoxID] = [bboxCoordinates[0] , bboxCoordinates[1] , bboxCoordinates[2] + (value.location.x - startLoc.x), bboxCoordinates[3] + (value.location.y - startLoc.y)]
-                        //                                }
-                        // works
+                    
                         else if value.startLocation.x >=  ((bboxCoordinates[0]-15) + bboxCoordinates[2]) && value.startLocation.x <= ((bboxCoordinates[0] + 15) + bboxCoordinates[2])  && value.startLocation.y >=  ((bboxCoordinates[1] - 15) + bboxCoordinates[3]) && value.startLocation.y <=  ((bboxCoordinates[1] +  15) + bboxCoordinates[3]){
                             rectData[boundingBoxID] = [bboxCoordinates[0] , bboxCoordinates[1] , bboxCoordinates[2] + (value.location.x - startLoc.x), bboxCoordinates[3] + (value.location.y - startLoc.y)]
-//                            rectCircleData[boundingBoxID] = [bboxCoordinates[0] , bboxCoordinates[1] , bboxCoordinates[2] + (value.location.x - startLoc.x), bboxCoordinates[3] + (value.location.y - startLoc.y)]
+                            
+//                            startLoc.x = bboxCoordinates[0]
+//                            startLoc.y = bboxCoordinates[1]
+//                            contWidth = bboxCoordinates[2] + (value.location.x - startLoc.x)
+//                            contHeight = bboxCoordinates[3] + (value.location.y - startLoc.y)
                         }
                         else{
                             continue
@@ -133,52 +130,52 @@ struct testViewNew: View {
                         bboxID += 1
                         print(rectCircleData)
                     }
-                    else{
-                        //                                print("start location: ", value.startLocation)
-                        var count_annotation_cords = 0
-                        for (boundingBoxID, bboxCoordinates) in rectCircleData{
-                            //                                    print("(\(key),\(value))")
-                            count_annotation_cords += 1
-                            
-                            if value.startLocation.x >=  (bboxCoordinates[0]-15) && value.startLocation.x <= ( bboxCoordinates[0] +  15)  && value.startLocation.y >=  (bboxCoordinates[1] - 15) && value.startLocation.y <= ( bboxCoordinates[1] +  15){
-                                continue
-                                //                                        startLoc.x = bboxCoordinates[0] - (-1 * (value.location.x - startLoc.x))
-                                //                                        startLoc.y = bboxCoordinates[1] - (-1 * (value.location.y - startLoc.y))
-                                //                                        contWidth = bboxCoordinates[2] + (-1 * (value.location.x - startLoc.x))
-                                //                                        contHeight = bboxCoordinates[3] + (-1 * (value.location.y - startLoc.y))
-                            }
-                            else if value.startLocation.x >=  ((bboxCoordinates[0]-15) + bboxCoordinates[2]) && value.startLocation.x <= ((bboxCoordinates[0] + 15) + bboxCoordinates[2])  && value.startLocation.y >=  (bboxCoordinates[1] - 15) && value.startLocation.y <=  (bboxCoordinates[1] +  15){
-                                print("Coordinate C2 clicked", startLoc.x, bboxCoordinates[0] + bboxCoordinates[2])
-                            }
-                            else if value.startLocation.x >=  (bboxCoordinates[0]-15) && value.startLocation.x <= (bboxCoordinates[0] + 15)   && value.startLocation.y >=  ((bboxCoordinates[1] - 15) + bboxCoordinates[3]) && value.startLocation.y <=  ((bboxCoordinates[1] +  15) + bboxCoordinates[3]){
-                                print("Coordinate C3 clicked", startLoc.x, bboxCoordinates[0] + bboxCoordinates[2])
-                            }
-                            else if value.startLocation.x >=  ((bboxCoordinates[0]-15) + bboxCoordinates[2]) && value.startLocation.x <= ((bboxCoordinates[0] + 15) + bboxCoordinates[2])  && value.startLocation.y >=  ((bboxCoordinates[1] - 15) + bboxCoordinates[3]) && value.startLocation.y <=  ((bboxCoordinates[1] +  15) + bboxCoordinates[3]){
-                                print("Coordinate C4 clicked", startLoc.x, bboxCoordinates[0] + bboxCoordinates[2])
-                            }
-                            else{
-                                continue
-                            }
-                            if value.startLocation.x >=  bboxCoordinates[0] && value.startLocation.x <= ( bboxCoordinates[0] +  bboxCoordinates[2])  && value.startLocation.y >=  bboxCoordinates[1] && value.startLocation.y <= ( bboxCoordinates[1] +  bboxCoordinates[3]){
-                                print("value within selected bbox")
-                                print(count_annotation_cords, boundingBoxID) // this is where we check if the clicked coordinates are within an existing bounding box
-                            }
-                            else{
-                                continue
-                            }
-                        }
-                        //                                    for cords in rectData{
-                        //                                        count_annotation_cords += 1
-                        //
-                        //                                        if value.startLocation.x >= cords[0] && value.startLocation.x <= (cords[0] + cords[2])  && value.startLocation.y >= cords[1] && value.startLocation.y <= (cords[1] + cords[3]){
-                        //                                            print("value within selected bbox")
-                        //                                            print(count_annotation_cords)
-                        //                                        }
-                        //                                        else{
-                        //                                            continue
-                        //                                        }
-                        //                                    }
-                    }
+//                    else{
+//                        //                                print("start location: ", value.startLocation)
+//                        var count_annotation_cords = 0
+//                        for (boundingBoxID, bboxCoordinates) in rectCircleData{
+//                            //                                    print("(\(key),\(value))")
+//                            count_annotation_cords += 1
+//
+//                            if value.startLocation.x >=  (bboxCoordinates[0]-15) && value.startLocation.x <= ( bboxCoordinates[0] +  15)  && value.startLocation.y >=  (bboxCoordinates[1] - 15) && value.startLocation.y <= ( bboxCoordinates[1] +  15){
+//                                continue
+//                                //                                        startLoc.x = bboxCoordinates[0] - (-1 * (value.location.x - startLoc.x))
+//                                //                                        startLoc.y = bboxCoordinates[1] - (-1 * (value.location.y - startLoc.y))
+//                                //                                        contWidth = bboxCoordinates[2] + (-1 * (value.location.x - startLoc.x))
+//                                //                                        contHeight = bboxCoordinates[3] + (-1 * (value.location.y - startLoc.y))
+//                            }
+//                            else if value.startLocation.x >=  ((bboxCoordinates[0]-15) + bboxCoordinates[2]) && value.startLocation.x <= ((bboxCoordinates[0] + 15) + bboxCoordinates[2])  && value.startLocation.y >=  (bboxCoordinates[1] - 15) && value.startLocation.y <=  (bboxCoordinates[1] +  15){
+//                                print("Coordinate C2 clicked", startLoc.x, bboxCoordinates[0] + bboxCoordinates[2])
+//                            }
+//                            else if value.startLocation.x >=  (bboxCoordinates[0]-15) && value.startLocation.x <= (bboxCoordinates[0] + 15)   && value.startLocation.y >=  ((bboxCoordinates[1] - 15) + bboxCoordinates[3]) && value.startLocation.y <=  ((bboxCoordinates[1] +  15) + bboxCoordinates[3]){
+//                                print("Coordinate C3 clicked", startLoc.x, bboxCoordinates[0] + bboxCoordinates[2])
+//                            }
+//                            else if value.startLocation.x >=  ((bboxCoordinates[0]-15) + bboxCoordinates[2]) && value.startLocation.x <= ((bboxCoordinates[0] + 15) + bboxCoordinates[2])  && value.startLocation.y >=  ((bboxCoordinates[1] - 15) + bboxCoordinates[3]) && value.startLocation.y <=  ((bboxCoordinates[1] +  15) + bboxCoordinates[3]){
+//                                print("Coordinate C4 clicked", startLoc.x, bboxCoordinates[0] + bboxCoordinates[2])
+//                            }
+//                            else{
+//                                continue
+//                            }
+//                            if value.startLocation.x >=  bboxCoordinates[0] && value.startLocation.x <= ( bboxCoordinates[0] +  bboxCoordinates[2])  && value.startLocation.y >=  bboxCoordinates[1] && value.startLocation.y <= ( bboxCoordinates[1] +  bboxCoordinates[3]){
+//                                print("value within selected bbox")
+//                                print(count_annotation_cords, boundingBoxID) // this is where we check if the clicked coordinates are within an existing bounding box
+//                            }
+//                            else{
+//                                continue
+//                            }
+//                        }
+//                        //                                    for cords in rectData{
+//                        //                                        count_annotation_cords += 1
+//                        //
+//                        //                                        if value.startLocation.x >= cords[0] && value.startLocation.x <= (cords[0] + cords[2])  && value.startLocation.y >= cords[1] && value.startLocation.y <= (cords[1] + cords[3]){
+//                        //                                            print("value within selected bbox")
+//                        //                                            print(count_annotation_cords)
+//                        //                                        }
+//                        //                                        else{
+//                        //                                            continue
+//                        //                                        }
+//                        //                                    }
+//                    }
                 })
             
             let simultaneously = pressGesture.simultaneously(with: mainGesture)
@@ -210,7 +207,7 @@ struct testViewNew: View {
                         )
                         )
                         .stroke(Color(red: 1.0, green: 0.78, blue: 0.16), lineWidth: 3.0)
-//                        .offset(offset)
+                    //                        .offset(offset)
                     Circle()
                         .fill(.yellow)
                         .frame(width: 15, height: 15)
