54c176bc93e86a7e41429f0edd029be9c954539a
jithin8mathew <jithinjosemathewoo8@gmail.com>
2022-08-31 12:50:45 -0500

    new interface

--- finalView.swift
+++ finalView.swift
@@ -13,8 +13,6 @@ struct finalView: View {
         case dragging(translation: CGSize) // draging for bbox annotation, either it is in open space or within already drawn bbox
         case longPressing // long pressing is only for dragging an already drawn bbox
         case inactive // all other time when screen is inactive
-//        case withinBbox
-//        case isCorner
         
         var translation: CGSize {
             switch self {
@@ -22,15 +20,47 @@ struct finalView: View {
                 return .zero
             case .dragging(let translation):
                 return translation
-//            case .withinBbox:
-//                return true
-//            case .isCorner:
-//                return true
+                //            case .withinBbox:
+                //                return true
+                //            case .isCorner:
+                //                return true
+            }
+        }
+        
+        var isActive: Bool {
+            switch self {
+            case .inactive:
+                return false
+            case .longPressing, .dragging:
+                return true
+            }
+        }
+        
+        var isDragging: Bool {
+            switch self {
+            case .inactive, .longPressing:
+                return false
+            case .dragging:
+                return true
             }
         }
     }
+    
+    enum AnnotatoinLocation {
+        case withinBbox
+        case isCorner
         
-        
+        var withinBBox: Bool{
+            switch self {
+            case .withinBbox:
+                return true
+            case .isCorner:
+                return true
+
+            }
+        }        
+    }
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
