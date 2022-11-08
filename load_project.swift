dacbe86c04eab8922f64d17480b0ad6bdb74dda2
jithin8mathew <jithinjosemathewoo8@gmail.com>
2022-11-08 02:17:07 -0600

    updates.

---
+++ load_project.swift
@@ -0,0 +1,23 @@
+//
+//  load_project.swift
+//  TactusNota
+//
+//  Created by Jithin  Mathew on 11/8/22.
+//
+
+import SwiftUI
+
+struct load_project: View {
+    var body: some View {
+        ZStack{
+            Color(red: 0.26, green: 0.26, blue: 0.26)
+            .ignoresSafeArea()
+        }
+    }
+}
+
+struct load_project_Previews: PreviewProvider {
+    static var previews: some View {
+        load_project()
+    }
+}
