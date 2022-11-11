518744f3e7b928a89ac7a3055a47c6ca9943f4b9
jithin8mathew <jithinjosemathewoo8@gmail.com>
2022-11-11 14:24:01 -0600

    updates

--- filePicker.swift
+++ filePicker.swift
@@ -10,24 +10,25 @@ struct filePicker: UIViewControllerRepresentable{
 
     @Binding var fileContent: String
 
-    func makeCoordinator() -> DocumentPickerCoordinator {
-        return DocumentPickerCoordinator(fileContent: $fileContent)
+    func makeCoordinator() -> filePickerCoordinator {
+        return filePickerCoordinator(fileContent: $fileContent)
     }
 
-    func makeUIViewController(context: UIViewControllerRepresentableContext<DocumentPicker>) -> UIDocumentPickerViewController{
+    func makeUIViewController(context: UIViewControllerRepresentableContext<filePicker>) -> UIDocumentPickerViewController{
         let controller: UIDocumentPickerViewController
 
+//        if #available(iOS 14, *) {...} else {...}
         controller.delegate = context.coordinator
         return controller
     }
 
-    func updateUIViewController(_ uiViewController: UIDocumentPickerViewController, context: UIViewControllerRepresentableContext<DocumentPicker>) {
+    func updateUIViewController(_ uiViewController: UIDocumentPickerViewController, context: UIViewControllerRepresentableContext<filePicker>) {
     }
 
 
 }
 
-class filePickerController: NSObject, UIDocumentPickerDelegate, UINavigationControllerDelegate{
+class filePickerCoordinator: NSObject, UIDocumentPickerDelegate, UINavigationControllerDelegate{
     
     @Binding var fileContent: String
     
@@ -35,7 +36,7 @@ class filePickerController: NSObject, UIDocumentPickerDelegate, UINavigationCont
         _fileContent = fileContent
     }
     
-    func documentPicker(_ controller: UIDocumentPickerViewController, didPickDocumentsAt urls: [URL]) {
+    func filePicker(_ controller: UIDocumentPickerViewController, didPickDocumentsAt urls: [URL]) {
         let fileURL = urls[0]
         do {
             fileContent = try String(contentsOf: fileURL, encoding: .utf8)
