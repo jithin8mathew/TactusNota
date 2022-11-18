0481be1d173d6eb72a188cf7411982e1729df738
jithin8mathew <jithinjosemathewoo8@gmail.com>
2022-11-18 01:03:20 -0600

    temp

--- image_picker.swift
+++ image_picker.swift
@@ -24,45 +24,6 @@ struct image_picker: View {
         
 }
 
-//struct DocumentPicker: UIViewControllerRepresentable{
-//
-//    @Binding var fileContent: String
-//
-//    func makeCoordinator() -> DocumentPickerCoordinator {
-//        return DocumentPickerCoordinator(fileContent: $fileContent)
-//    }
-//
-//    func makeUIViewController(context: UIViewControllerRepresentableContext<DocumentPicker>) -> UIDocumentPickerViewController{
-//        let controller: UIDocumentPickerViewController
-//
-//        controller.delegate = context.coordinator
-//        return controller
-//    }
-//
-//    func updateUIViewController(_ uiViewController: UIDocumentPickerViewController, context: UIViewControllerRepresentableContext<DocumentPicker>) {
-//    }
-//
-//
-//}
-
-//class DocumentPickerController: NSObject, UIDocumentPickerDelegate, UINavigationControllerDelegate{
-//
-//    @Binding var fileContent: String
-//
-//    init(fileContent: Binding<String>){
-//        _fileContent = fileContent
-//    }
-//
-//    func documentPicker(_ controller: UIDocumentPickerViewController, didPickDocumentsAt urls: [URL]) {
-//        let fileURL = urls[0]
-//        do {
-//            fileContent = try String(contentsOf: fileURL, encoding: .utf8)
-//        } catch let error{
-//            print(error.localizedDescription)
-//        }
-//    }
-//
-//}
 
 struct image_picker_Previews: PreviewProvider {
     static var previews: some View {
