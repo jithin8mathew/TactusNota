a1eaf75c496cb68177f34d596a57214da9ee345c
jithin8mathew <jithinjosemathewoo8@gmail.com>
2022-12-05 23:55:40 -0600

    still working on documents.

--- TactusNota/AnnotationModel.swift
+++ TactusNota/AnnotationModel.swift
@@ -12,13 +12,21 @@ class AnnotationModel: ObservableObject {
     @Published var items: [Item] = []
     
     init() {
-        if let documentDirectory = FileManager.documentDirectory {
-            let urls = FileManager.contentsOfDirectory(documentDirectory).filter { $0.isImage }
-            for url in urls {
-                let item = Item(url: url)
-                items.append(item)
-            }
+        let documentDirectoryURL = getDocumentsDirectory()
+        do {
+            let files = try FileManager.default.contentsOfDirectory(atPath: "/Users/username/Documents/Photos")
+
+            print(files)
+        } catch {
+            print(error)
         }
+//        if let documentDirectory = FileManager.default.documentDirectory {
+//         if let urls = FileManager.contentsOfDirectory(documentDirectoryURL).filter { $0.isImage }
+//            for url in urls {
+//                let item = Item(url: url)
+//                items.append(item)
+//            }
+//        }
         
         if let urls = Bundle.main.urls(forResourcesWithExtension: "jpg", subdirectory: nil) {
             for url in urls {
@@ -32,12 +40,18 @@ class AnnotationModel: ObservableObject {
     }
     
     /// Removes an item from the data collection.
-    func removeItem(_ item: Item) {
-        if let index = items.firstIndex(of: item) {
-            items.remove(at: index)
-            FileManager.default.removeItemFromDocumentDirectory(url: item.url)
-        }
+//    func removeItem(_ item: Item) {
+//        if let index = items.firstIndex(of: item) {
+//            items.remove(at: index)
+//            FileManager.default.removeItemFromDocumentDirectory(url: item.url)
+//        }
+//    }
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        let documentsDirectory = paths[0]
+        return documentsDirectory
     }
+    
 }
 
 extension URL {
