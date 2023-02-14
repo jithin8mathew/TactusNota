0f9e51d5a80f39896d3f07004ed4af67bce41ff2
jithin8mathew <jithinjosemathewoo8@gmail.com>
2023-02-14 00:10:24 -0600

    added an image picker.

--- TactusNota/AnnotationView.swift
+++ TactusNota/AnnotationView.swift
@@ -623,6 +623,79 @@ func presentImage(url: URL) -> UIImage{
 //    }
 //}
 
+//class imageViewModel: ObservableObject{
+//
+//    static let sharedIV = imageViewModel()
+//
+//    @Published var imageData: Data = Data(count: 0)
+//
+//}
+
+
+//struct ImagePicker2: UIViewControllerRepresentable {
+//
+//    var sourceType: UIImagePickerController.SourceType = .photoLibrary
+//    @Environment(\.presentationMode) var presentationMode
+//    @EnvironmentObject private var model : imageViewModel
+//
+//    @Binding var imageData : Data
+//
+//    func makeUIViewController(context: UIViewControllerRepresentableContext<ImagePicker>) -> UIImagePickerController {
+//
+//        let imagePicker = UIImagePickerController()
+//        imagePicker.allowsEditing = false
+//        imagePicker.sourceType = sourceType
+//        imagePicker.delegate = context.coordinator
+//        model.status = true
+//
+//        return imagePicker
+//    }
+//
+//    func updateUIViewController(_ uiViewController: UIImagePickerController, context: UIViewControllerRepresentableContext<ImagePicker>) {
+//
+//    }
+//
+//    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+//
+//    }
+//
+//    func makeCoordinator() -> Coordinator {
+//        Coordinator(self)
+//    }
+//
+//}
+
+
+
+
+//final class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+//
+//    var parent: ImagePicker2
+////    @IBOutlet weak var imageView: UIImageView!
+//    private var image2 : UIImage?
+//    @EnvironmentObject private var model : imageViewModel
+//
+//
+////    var imageView: UIImageView!
+//
+//    init(_ parent: ImagePicker2) {
+//        self.parent = parent
+//    }
+//
+//    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+//
+//        if let image = (info[UIImagePickerController.InfoKey.originalImage] as? UIImage)?.pngData() {
+//            parent.imageData = image
+//        }
+//
+//        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+//            imageProcessing = image
+//        }
+//        parent.presentationMode.wrappedValue.dismiss()
+//    }
+//
+//}
+
 struct AnnotationView_Previews: PreviewProvider {
     static var previews: some View {
         AnnotationView()
