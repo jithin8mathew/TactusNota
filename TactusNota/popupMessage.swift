0de70f82de56108a6c441b77704739e0a7c13378
jithin8mathew <jithinjosemathewoo8@gmail.com>
2023-03-06 11:58:10 -0600

    minor updates.

--- TactusNota/popupMessage.swift
+++ TactusNota/popupMessage.swift
@@ -10,11 +10,20 @@ import SwiftUI
 struct popupMessage: View {
     var body: some View {
         VStack(spacing: 0){
-            Text("warning")
+            Image(systemName: "gear")
+                .resizable()
+                .frame(width: 40, height: 40, alignment: .center)
+                .padding()
+                .foregroundColor(.white)
+                
+            Text("This is a waarning suggesting that \n the system was not able to load the image \n from the folder selected by the user")
+                .foregroundColor(.white)
             }
             .padding()
             .multilineTextAlignment(.center)
-            .background(.purple)
+            .background(.red)
+            .cornerRadius(10)
+//        Button()
     }
 }
 
