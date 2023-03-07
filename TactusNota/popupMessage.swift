1b3b9b2bb8a0efee01115f57515ba78207edba32
jithin8mathew <jithinjosemathewoo8@gmail.com>
2023-03-07 12:01:16 -0600

    made some progress on pop up message.

--- TactusNota/popupMessage.swift
+++ TactusNota/popupMessage.swift
@@ -10,20 +10,49 @@ import SwiftUI
 struct popupMessage: View {
     var body: some View {
         VStack(spacing: 0){
-            Image(systemName: "gear")
+            Image(systemName: "exclamationmark.triangle.fill")
                 .resizable()
                 .frame(width: 40, height: 40, alignment: .center)
                 .padding()
                 .foregroundColor(.white)
-                
+            
             Text("This is a waarning suggesting that \n the system was not able to load the image \n from the folder selected by the user")
                 .foregroundColor(.white)
-            }
+            //                .font(.custom("Playfair Display", fixedSize: 15))
+            
+            HStack{
+                Button(action: {}
+                ){
+                    Label("Accept", systemImage: "checkmark.circle")
+                        .foregroundColor(.black)
+                        .padding()
+                        .background(Color.yellow)
+                        .cornerRadius(50.0, corners: [.topLeft, .bottomLeft])
+                        .padding(.leading, -8)
+                        .frame(height: 54)
+                }
+                
+                Button(action: {}
+                ){
+                    Label("Decline", systemImage: "multiply.circle")
+                        .foregroundColor(.black)
+                        .padding()
+                        .background(Color.green)
+                        .cornerRadius(50.0, corners: [.topRight, .bottomRight])
+                        .padding(.leading, -8)
+                        .frame(height: 54)
+                }
+            } // end of button Hstack
             .padding()
-            .multilineTextAlignment(.center)
-            .background(.red)
-            .cornerRadius(10)
-//        Button()
+            .shadow(color: Color(red: 0.36, green: 0.36, blue: 0.36), radius: 8, x: 5, y: 5)
+        } // End of Vstack
+        .padding()
+        .multilineTextAlignment(.center)
+        .background(.red)
+        .cornerRadius(10)
+        .shadow(color: Color(red: 0.16, green: 0.16, blue: 0.16), radius: 15, x: 5, y: 5)
+        
+        //        Button()
     }
 }
 
